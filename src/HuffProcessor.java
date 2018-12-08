727929c92a0d7ffaabd7d7ce63b030e343889b1e
Abinaya Lakshmanan <al325@duke.edu>
2018-12-08 15:37:37 -0500

    Finished Decompress

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  * Although this class has a history of several years,
@@ -42,12 +43,81 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root,out);
+		
+		in.reset();
+		writeCompressedBits(codings,in,out);
 		out.close();
+		
+	}
+	
+	public int[] readForCounts(BitInputStream in) {
+		int[] freqs = new int[ALPH_SIZE + 1];
+		while(true) {
+			int bits = in.readBits(BITS_PER_WORD);
+			if (bits == -1) {
+				break;
+			}
+			else {
+				freqs[bits] = 1 + freqs[bits];
+			}
+		}
+		freqs[PSEUDO_EOF] = 1;
+		return freqs; 
+	}
+	
+	public HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		
+		for (int i=0; i< counts.length; i++) {
+			if (counts[i] > 0) {
+				pq.add(new HuffNode(i,counts[i],null,null));
+			}
+		}
+		
+		while (pq.size() > 1) {
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(left.myValue + right.myValue,right.myWeight + left.myWeight, left,right);
+			pq.add(t);
+		}
+		HuffNode root = pq.remove();
+		return root;
+	}
+	
+	public String[] makeCodingsFromTree(HuffNode root) {
+		String[] encodings = new String[ALPH_SIZE + 1];
+		codingHelper(root, "",encodings);
+		return encodings;
+		}
+	
+	private void codingHelper(HuffNode n, String path, String[] encode) {
+		if (n == null) {
+			return;
+		}
+		else if (n.myLeft == null && n.myRight == null) {
+			encode[n.myValue] = path;
+			return;
+		}
+		else {
+			codingHelper(n.myLeft,path+"0",encode);
+			codingHelper(n.myRight,path+"1",encode);
+		}
+	}
+	
+	public void writeHeader(HuffNode root, BitOutputStream out) {
+		if (root.myRight == null && root.myLeft == null) {
+			
+		}
+	}
+	
+	public void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
+		
 	}
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
@@ -59,12 +129,61 @@ public class HuffProcessor {
 	 *            Buffered bit stream writing to the output file.
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
-
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		
+		int bits = in.readBits(BITS_PER_INT);
+		if(bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with "+bits);
 		}
+		if(bits == -1) {
+			throw new HuffException("reading bits failed"+bits);
+		}
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root,in,out);
 		out.close();
+	}
+	public HuffNode readTreeHeader(BitInputStream in1) {
+		
+		while (true) {
+			int bit = in1.readBits(1);
+			if (bit == -1) {
+				throw new HuffException("bad input");
+			}
+			if (bit == 0) {
+				HuffNode left = readTreeHeader(in1);
+				HuffNode right = readTreeHeader(in1);
+				return new HuffNode(0,0,left,right);
+			}
+			else {
+				int value = in1.readBits(BITS_PER_WORD + 1);
+				return new HuffNode(value,0,null,null);
+			}
+		}
+	}
+		
+	public void readCompressedBits(HuffNode n, BitInputStream in2, BitOutputStream out) {
+		HuffNode current = n;
+		while (true) {
+			int bits = in2.readBits(1);
+			if (bits == -1) {
+				throw new HuffException("bad input, no PSEUDO_EOF");
+			}
+			else {
+				if (bits == 0) {
+					current = current.myLeft;
+				}
+				else {
+					current = current.myRight;
+				}
+				if (current.myLeft == null && current.myRight == null ) {
+					if (current.myValue == PSEUDO_EOF) {
+						break;
+					}
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = n;
+					}
+				}
+			}
+		}
 	}
 }
